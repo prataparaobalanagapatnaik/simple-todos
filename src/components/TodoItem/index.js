f9ac0d40b5de4dfd56007ea763e66d7d7ed931a7
patnaik <prataparaopatnaik@gmail.com>
2024-10-04 15:44:09 +0000

    Enhancement of simple todos

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,19 +1,65 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
 
-import React from 'react';
-import './index.css';
+class TodoItem extends Component {
+  state = {
+    editing: false,
+    updatedTitle: '',
+  }
 
-const TodoItem = ({ todo, deleteTodo }) => {
-  const { id, title } = todo;
+  handleEdit = () => {
+    const {todoDetails} = this.props
+    this.setState({editing: true, updatedTitle: todoDetails.title})
+  }
 
-  return (
-    <li className="todo-item">
-      <p className="todo-title">{title}</p>
-      <button className="delete-button" onClick={() => deleteTodo(id)}>
-        Delete
-      </button>
-    </li>
-  );
-};
+  handleSave = () => {
+    // const {todoDetails} = this.props
+    // const {updatedTitle} = this.state
+    this.setState({editing: false})
+    // Call a function to save updated title (not implemented in this code)
+  }
 
-export default TodoItem;
+  handleChange = e => {
+    this.setState({updatedTitle: e.target.value})
+  }
+
+  render() {
+    const {todoDetails, deleteTodo, toggleComplete} = this.props
+    const {editing, updatedTitle} = this.state
+    return (
+      <li
+        className={todoDetails.completed ? 'todo-item completed' : 'todo-item'}
+      >
+        {editing ? (
+          <>
+            <input
+              type="text"
+              value={updatedTitle}
+              onChange={this.handleChange}
+            />
+            <button onClick={this.handleSave} type="button">
+              Save
+            </button>
+          </>
+        ) : (
+          <>
+            <input
+              type="checkbox"
+              checked={todoDetails.completed}
+              onChange={() => toggleComplete(todoDetails.id)}
+            />
+            <p className="title">{todoDetails.title}</p>
+            <button onClick={this.handleEdit} type="button">
+              Edit
+            </button>
+            <button onClick={() => deleteTodo(todoDetails.id)} type="button">
+              Delete
+            </button>
+          </>
+        )}
+      </li>
+    )
+  }
+}
+
+export default TodoItem
